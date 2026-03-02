089fc5b9e0b81a57ea95f0beff7efa2bd5f32690
danielalamo23 <alamocifuentesdaniel@gmail.com>
2026-03-02 20:30:01 +0100

    Add files via upload
    Codigo

---
+++ GestionDeReservas.java
@@ -0,0 +1,37 @@
+public class GestionDeReservas {
+    /*
+    El sistema debe permitir:
+
+    1. Registrar una nueva reserva
+    2. Buscar una reserva por su número
+    3. Mostrar todas las reservas
+    4. Consultar el libro asociado a una reserva
+
+    Reglas del sistema (IMPORTANTE)
+
+    1. No se puede registrar una reserva con un número que ya exista.
+    2. No se puede buscar una reserva que no exista.
+
+    El sistema debe disponer de un componente de acceso a datos encargado de:
+
+    1. Guardar reservas
+    2. Buscar reservas
+    3. Listar reservas
+    */
+
+    public static void registrarReserva(){
+
+    }
+    
+    public static void buscarReserva(){
+
+    }
+
+    public static void mostrarReservas(){
+
+    }
+
+    public static void consultarLibro(){
+        
+    }
+}
